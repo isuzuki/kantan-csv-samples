afe02ab079f05fafd365d1b9234ab6acb4b86c1f
isuzuki <ikkuntech@gmail.com>
2018-12-06 01:48:44 +0900

    move case class definition to nested object inner

--- src/main/scala/io/github/isuzuki/sample/NestedRead.scala
+++ src/main/scala/io/github/isuzuki/sample/NestedRead.scala
@@ -9,16 +9,17 @@ import kantan.csv.ops._
 case class Item (
   id: Int,
   name: String,
-  spec1: Spec,
-)
-
-case class Spec (
-  name: String,
-  value: Int
+  spec1: Item.Spec,
 )
 
 object Item {
   implicit lazy val headerDecoder: HeaderDecoder[Item] = HeaderDecoder.defaultHeaderDecoder[Item]
+  implicit lazy val rowDecoder: RowDecoder[Spec] = RowDecoder.decoder(0, 1)(Spec.apply)
+
+  case class Spec (
+    name: String,
+    value: Int
+  )
 }
 
 object NestedRead extends App {
